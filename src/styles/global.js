fd5c5cb2bfe39de28efec5ee6c659b765cc3b3fb
Rian-Fernandes <rian10fernandes@hotmail.com>
2023-11-02 23:09:37 -0300

    Editando imagem de fundo

--- src/styles/global.js
+++ src/styles/global.js
@@ -11,7 +11,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     width: 100vw;
     height: 100vh;
-    background-image: url('https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c8a70d92332465.5e490718ed693.gif');
+    background-image: url('https://img.freepik.com/vetores-gratis/fundo-roxo-do-ceu-nublado-com-grupo-de-cumulos-e-nuvens-cirros-ilustracao-plana-dos-desenhos-animados_1284-62768.jpg?w=900&t=st=1698942788~exp=1698943388~hmac=d246cd853b38d563c397b49fc050b7dc034e3310003e5b7c23d8cbbd535a52d0');
     background-size: cover; // Ajuste o tamanho da imagem conforme necessário
     font-family: Arial, Helvetica, sans-serif;
   }
